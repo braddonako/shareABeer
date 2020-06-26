243c154fb9ca4481f70fc0fff9738383477f962b
Brad Donakowski <braddonako@gmail.com>
2020-06-26 12:04:02 -0600

    trying to connect our server

---
+++ src/schema/index.js
@@ -0,0 +1,17 @@
+ import noteSchema from "./notes";
+
+ const linkSchema = gql `
+      type Query {
+        _: Boolean
+      }
+    
+      type Mutation {
+        _: Boolean
+      }
+    
+      type Subscription {
+        _: Boolean
+      }
+    `;
+
+ export default [linkSchema, noteSchema];
